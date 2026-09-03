1125f37c8878e03da6b753519bfaf9ee96db2716
JuanHarvey27 <juanpat27@gmail.com>
2026-09-03 16:09:26 -0500

    The Parallelogram class was created and developed on the backend.

---
+++ LogicConceptsInheritance/Backend/Parallelogram.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace Backend;
+public class Parallelogram : Rectangle
+{
+    // Fields 
+    private double _h;
+    public Parallelogram(string name, double a, double b, double h) : base(name, a, b)
+    {
+        H = _h;
+    }
+
+    // Properties
+
+    public double H
+    {
+        get => _h;
+        set => _h = ValidateH(value);
+    }
+
+    // Public Methods
+
+    public override double GetArea()
+    {
+        double Area = A * H;
+        return Area;
+    }
+    public override double GetPerimeter()
+    {
+        double Perimeter = 2 * (A + B);
+        return Perimeter;
+    }
+
+    // Private Methods
+
+    private double ValidateH(double H)
+    {
+        if (H <= 0)
+        {
+            throw new ArgumentException("Height must be greater than zero.");
+        }
+        return H;
+    }
+}
--- LogicConceptsInheritance/Backend/Rectangle.cs
+++ LogicConceptsInheritance/Backend/Rectangle.cs
@@ -1,5 +1,4 @@
-﻿using System;
-namespace Backend;
+﻿namespace Backend;
 
 public class Rectangle : Square
 {
