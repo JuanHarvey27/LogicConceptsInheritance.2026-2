19d95a91cc200f06494c4f7900a24fc04e569733
JuanHarvey27 <juanpat27@gmail.com>
2026-09-03 13:52:09 -0500

    The front end and the first prototype of the GeometricFigure class in the back end were developed

---
+++ LogicConceptsInheritance/Backend/GeometricFigures.cs
@@ -0,0 +1,24 @@
+﻿namespace Backend;
+
+public abstract class GeometricFigure
+{
+
+    // Constructors
+    protected GeometricFigure(string name)
+    {
+        Name = name;
+    }
+    // Properties
+    public string Name { get; set; } = null!;
+
+    // Public Methods
+    public abstract double GetArea();
+
+    public abstract double GetPerimeter();
+
+    public override string ToString()
+    {
+        return $"{Name} => Area: {GetArea():F10}, Perimeter: {GetPerimeter():F10}";
+    }
+
+}
