07b4a853e2654133c04c59134de7316919781c91
JuanHarvey27 <juanpat27@gmail.com>
2026-09-03 14:22:04 -0500

    The Circle back end class was created

---
+++ LogicConceptsInheritance/Backend/Circle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace Backend;
+
+public class Circle : GeometricFigure
+{
+    // Fields 
+    private double _r;
+    // Constructors
+    public Circle(string name, double r) : base(name)
+    {
+        R = r;
+    }
+    // Properties
+    public double R 
+    { 
+        get => _r; 
+        set =>_r = ValidateR(value);
+    }
+
+    // Public Methods
+    public override double GetArea()
+    {
+        double Area = Math.PI * Math.Pow(R, 2);
+        return Area;
+    }
+
+    public override double GetPerimeter()
+    {
+        double Perimeter = 2 * Math.PI * R;
+        return Perimeter;
+    }
+
+    // Private Methods
+
+    private double ValidateR(double r)
+    {
+        if (r <= 0)
+        {
+            throw new ArgumentException("Radius must be greater than zero.");
+        }
+        return r;
+    }
+}
